2695b5f7d407ac94ac935356302fa729eb1a3e47
BinLucky <87031880+BinLucky@users.noreply.github.com>
2022-08-17 12:38:07 +0300

    Validator Changes

--- pkg/validators/barcode_validator.go
+++ pkg/validators/barcode_validator.go
@@ -7,7 +7,7 @@ func IsBarcodeValid(barcode string) bool {
 	var rxBarcode = regexp.MustCompile("^[0-9]+$")
 	runeInBarcode := []rune(barcode)
 
-	if len(runeInBarcode) != 8 || rxBarcode.MatchString(barcode) {
+	if len(runeInBarcode) != 8 || !rxBarcode.MatchString(barcode) {
 		return false
 	}
 	return true
